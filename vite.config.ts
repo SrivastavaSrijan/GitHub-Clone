13b5101163d35bba8cc3bad288c8a867e6987fee
SrivastavaSrijan <srijan.srivastava35@gmail.com>
2024-08-10 13:14:18 +0530

    fix: env

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import checker from 'vite-plugin-checker';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), checker({ typescript: true })],
+  define: {
+    "process.env": process.env,
+    VITE_GIT_TOKEN: process.env.VITE_GIT_TOKEN,
+  },
 });
